785f7bd1d831e4b4c0d9735c912a8d48c5cc7766
f-mkotsovoulou <!CloudGit2020#>
2021-10-21 23:38:15 +0300

    changed routes

--- djone/first/urls.py
+++ djone/first/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path ("", views.index, name="index"),
+urlpatterns = [path("", views.flights, name="flights"),
                path ('maira', views.maira, name="maira"),
-               path('flights', views.flights, name="flights")
                ]
