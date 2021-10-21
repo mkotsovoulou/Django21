0877681f3e99d48e2d00ca5fd65708269e4e424e
f-mkotsovoulou <!CloudGit2020#>
2021-10-21 22:49:58 +0300

    flights

--- djone/first/views.py
+++ djone/first/views.py
@@ -5,9 +5,8 @@ from models import Flights
 def index(request):
     return HttpResponse("<h1> Hello World </h1>")
 
-def maira(request) :
+def maira(request):
     return HttpResponse("<h1> Hello Maira !!!</h1>")
-
 
 def flights(request):
     return render(request, "first/flights.html", {
