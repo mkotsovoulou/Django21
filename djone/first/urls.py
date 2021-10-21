2a6c31e50608e680688ba7f3299a87f42a7198b7
f-mkotsovoulou <!CloudGit2020#>
2021-10-21 22:45:30 +0300

    flights

--- djone/first/urls.py
+++ djone/first/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path ("", views.index, name="index"),
-               path ('maira', views.maira, name="maira")
+               path ('maira', views.maira, name="maira"),
+               path('flights', views.flights, name="flights")
                ]
