d89e31ee73e4d38e23dc9c2dbd9d5566c9b128ee
f-mkotsovoulou <!CloudGit2020#>
2021-10-21 22:51:24 +0300

    flights

--- djone/first/views.py
+++ djone/first/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from models import Flights
+from .models import Flights
+
 def index(request):
     return HttpResponse("<h1> Hello World </h1>")
 
