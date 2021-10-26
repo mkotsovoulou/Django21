446e2cf934e945bf801065e249fe2b199e0a6f8f
f-mkotsovoulou <!CloudGit2020#>
2021-10-26 16:07:18 +0300

    tasks app

--- djone/djone/urls.py
+++ djone/djone/urls.py
@@ -17,9 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('first.urls')),
-    path('flights/', include('first.urls')),
     path('admin/', admin.site.urls),
     path('first/', include ("first.urls")),
-    path('second/', include("second.urls"))
+    path('second/', include("second.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
