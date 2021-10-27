82d44c48e85065c5cc59b60f4fff6de3b6089f68
f-mkotsovoulou <!CloudGit2020#>
2021-10-27 10:54:52 +0300

    if

--- djone/tasks/views.py
+++ djone/tasks/views.py
@@ -4,11 +4,12 @@ from django.http import HttpResponse
 
 #global variables
 tasks = ["Cook", "Study", "Clean my Room", "Wash the Car", "Go to the supermarket"]
-
+devname = 'Maira';
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
-        "tasklist" : tasks
+        "tasklist" : tasks,
+        "devname" : devname
     })
 
 def add(request):
