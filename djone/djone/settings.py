c24af00cd73d76758f0cb36cce9375a7047eb494
f-mkotsovoulou <!CloudGit2020#>
2021-10-31 21:59:29 +0200

    heroku

--- djone/djone/settings.py
+++ djone/djone/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'files')
+
+# STATIC_ROOT = '/var/www/example.com/static/' FOR DEPLOYMENT
+# python manage.py collectstatic WILL COPY ALL FLES TO SERVER STATIC FOLDER called files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
